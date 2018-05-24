7491919e72701860dc83616f2b3ac8cc6a5c0963
albertoi <alberto.eps@gmail.com>
2018-05-24 21:00:47 +0200

    nueva ley proteccion de datos

--- app/views/info/proteccion-datos.php
+++ app/views/info/proteccion-datos.php
@@ -2,10 +2,19 @@
     <h2 class="subhead">POLÍTICA DE PRIVACIDAD </h2>
 
 
+    <h4 class="subhead">INFORMACIÓN BÁSICA SOBRE PROTECCIÓN DE DATOS</h4>
+    <ul>
+        <li>Responsable del tratamiento: Jorge Manuel Bueno Méndez</li>
+        <li>Finalidad: Atender sus peticiones.</li>
+        <li>Legitimación: Consentimiento.</li>
+        <li>Destinatarios: No se cederán datos a terceros, salvo obligación legal.</li>
+        <li>Derechos de los titulares de los datos: De acceso, rectificación, supresión, portabilidad, limitación del tratamiento, y de oposición al tratamiento, dirigiéndose al correo electrónico: <a href="mailto:central@areafriki.com">central@areafriki.com</a></li>
+    </ul>
+
 
     <h4 class="subhead">INFORMACIÓN GENERAL </h4>
 
-    <p>El presente texto contiene la política de privacidad (en adelante, la “Política de Privacidad”) aplicada a sus datos personales en el presente sitio web https://areafriki.com (en adelante, la “Web”), en cumplimiento de la normativa sobre protección de la privacidad, y en concreto, de conformidad con la Ley Orgánica 15/1999, de 13 de diciembre, de Protección de Datos de Carácter Personal y el Real Decreto 1720/2007, de 21 de diciembre que la desarrolla, así como otras normas análogas o complementarias. </p>
+    <p>El presente texto contiene la política de privacidad (en adelante, la “Política de Privacidad”) aplicada a sus datos personales en el presente sitio web https://areafriki.com (en adelante, la “Web”), en cumplimiento de la normativa sobre protección de la privacidad, y en concreto, de conformidad con el Reglamento 2016/679 de Protección de Datos (RGPD), así como otras normas análogas o complementarias.</p>
 
 
     <h4 class="subhead">DATOS IDENTIFICATIVOS </h4>
@@ -14,39 +23,41 @@
 
     <p>Jorge Manuel Bueno Mendez (en adelante, el “Titular de la Web”) </p>
     <p>Número de identificación: 80079483-S</p>
-    <p>Domicilio: C/Zurbarán, número 1, planta 2, oficina 3, 06002 Badajoz</p>
+    <p>Domicilio: C/ La Albuera, 59, 06010 Badajoz</p>
     <p>Email: central@areafriki.com</p>
 
 
     <h4 class="subhead">FINALIDAD DEL TRATAMIENTO DE LOS DATOS </h4>
 
-    <p>La Web recoge datos personales del usuario, bien recogidos automáticamente durante el uso de la Web, o proporcionados voluntariamente por el propio usuario en los apartados informados de la Web. Todos los datos personales recogidos, los incorporamos a ficheros responsabilidad del Titular de la Web. Dichos datos serán tratados con las finalidades siguientes: </p>
+    <p>La Web recoge datos personales del usuario, bien recogidos automáticamente durante el uso de la Web, o proporcionados voluntariamente por el propio usuario en los apartados informados de la Web. Todos los datos personales recogidos, los incorporamos a ficheros responsabilidad del Titular de la Web. Dichos datos serán tratados con las finalidades siguientes:</p>
 
-    <p>Para tener en cuenta su currículum vitae en los procesos de selección de personal. </p>
-    <p>Para efectuar el envío de comunicaciones publicitarias (en el supuesto que lo autorice expresamente en el momento oportuno) relacionadas con los productos y servicios ofrecidos en la Web. </p>
-    <p>Para gestionar los servicios y productos contratados a través de la Web. </p>
-    <p>Para mostrar la información del usuario en el perfil de su cuenta de la Web. </p>
-    <p>Para poder contactar con el usuario. </p>
-    <p>Para efectuar el registro como usuario en la Web. </p>
+    <ul>
+        <li>Para efectuar el envío de comunicaciones publicitarias (en el supuesto que lo autorice expresamente en el momento oportuno) relacionadas con los productos y servicios ofrecidos en la Web.</li>
+        <li>Para gestionar los servicios y productos contratados a través de la Web.</li>
+        <li>Para mostrar la información del usuario en el perfil de su cuenta de la Web.</li>
+        <li>Para poder contactar con el usuario.</li>
+        <li>Para efectuar el registro como usuario en la Web.</li>
+    </ul>
 
+    <p>La base jurídica para el tratamiento de sus datos es el consentimiento que usted nos facilita al ponerse en contacto con nosotros y facilitarnos sus datos personales.</p>
     <p>Antes de que nos facilite datos personales de terceros, debe haber recabado previamente su consentimiento respecto al contenido de la presente Política de Privacidad. </p>
-
-    <p>Si voluntariamente nos facilita datos de carácter personal, consiente tácitamente a que el Titular de la Web procese sus datos como aquí se detalla. En el caso en el que los datos personales facilitados por el usuario se utilicen con finalidades distintas a las enunciadas, se solicitará el consentimiento expreso del usuario. </p>
-
-    <p>Para recibir mayor información sobre sus datos personales y la finalidad de  su tratamiento, contacten con el Titular de la Web. </p>
+    <p>Si voluntariamente nos facilita datos de carácter personal, consiente tácitamente a que el Titular de la Web procese sus datos como aquí se detalla. En el caso en el que los datos personales facilitados por el usuario se utilicen con finalidades distintas a las enunciadas, se solicitará el consentimiento expreso del usuario.</p>
+    <p>Para recibir mayor información sobre sus datos personales y la finalidad de  su tratamiento, contacten con el Titular de la Web.</p>
+    <p>Los datos obtenidos se conservarán durante el tiempo requerido para atender su petición. </p>
 
 
     <h4 class="subhead">SEGURIDAD Y CUMPLIMIENTO </h4>
 
-    <p>El Titular de la Web tratará los datos de los usuarios de manera adecuada y responsable, y adoptará todas las medidas de índole técnica y organizativa que exige la legislación vigente en materia de protección de datos, a fin de evitar la alteración, pérdida, tratamiento o acceso no autorizado a los datos. </p>
+    <p>El Titular de la Web tratará los datos de los usuarios de manera adecuada y responsable, y adoptará todas las medidas de índole técnica y organizativa que exige la legislación vigente en materia de protección de datos, a fin de evitar la alteración, pérdida, tratamiento o acceso no autorizado a los datos.</p>
 
 
     <h4 class="subhead">SERVICIOS DE TERCEROS </h4>
 
-    <p>Al margen del Titular de la Web, en algunas circunstancias otras personas, bien de la propia organización de la empresa o subcontratistas externos (tales como, abogados, agencias de marketing, informáticos, administración, empresas de mensajería, auditores o agencias de comunicación), podrán acceder a los datos para cumplir con una adecuada prestación de los servicios al usuario. </p>
+    <p>No cederemos sus datos a terceros salvo obligación legal.</p>
 
 
     <h4 class="subhead">DERECHOS </h4>
 
-    <p>Puede ejercer sus derechos de acceso, rectificación, cancelación y oposición, remitiendo una comunicación escrita a la que deberá acompañar una copia de su documento nacional de identidad, pasaporte u otro documento válido que lo identifique, la petición que realiza, su dirección a efectos de notificaciones, la fecha y su firma, junto con los documentos que acrediten la petición, en su caso, a la dirección C/Zurbarán, número 1, planta 2, oficina 3, 06002 Badajoz, o bien enviando un correo electrónico a central@areafriki.com. </p>
+    <p>Puede ejercer sus derechos de acceso, rectificación, supresión, portabilidad, limitación y oposición del tratamiento, remitiendo una comunicación escrita a la que deberá acompañar una copia de su documento nacional de identidad, pasaporte u otro documento válido que lo identifique, la petición que realiza, su dirección a efectos de notificaciones, la fecha y su firma, junto con los documentos que acrediten la petición, en su caso, a la dirección C/ La Albuera, 59, o bien enviando un correo electrónico a central@areafriki.com.</p>
+    <p>Asimismo, también tiene derecho a presentar una reclamación ante la Agencia Española de Protección de datos si no está satisfecho con la atención de sus derechos. </p>
 </div>
